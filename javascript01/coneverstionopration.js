b1292e408a10eeea8cc94036b7966ef2c99bd00e
Pratik Gaikwad <114159783+pratikbgaikwad@users.noreply.github.com>
2023-01-19 10:54:51 +0000

    yes

--- javascript01/coneverstionopration.js
+++ javascript01/coneverstionopration.js
@@ -22,6 +22,45 @@ let booleanIsloggedIn = Boolean(isLoggedIn)
 
 let someNumber = 33
 
+
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// ****************************** opration *********************************
+
+let value = 3
+let negvalue = -value 
+// console.log(negvalue);
+
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2%2);
+
+
+let str1 ="hello"
+let str2 = " praitk"
+ 
+let str3 = str1+str2
+console.log(str3);
+
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log(1+"2"+2);
+// console.log(1+2+"2");
+
+// console.log( (3 + 4) * 5 % 3);
+
+console.log(+true);//1
+console.log(+""); //0
+
+let num1, num2, num3
+
+num1 = num2 = num3 =2 + 2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
