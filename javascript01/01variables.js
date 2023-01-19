3e2fab232746748e69c85248f9dde4397a738bd7
Pratik Gaikwad <114159783+pratikbgaikwad@users.noreply.github.com>
2023-01-19 09:15:12 +0000

    yes datatype

---
+++ javascript01/01variables.js
@@ -0,0 +1,21 @@
+const accountId = 144434
+let accountemail = "pratik@gmail.com"
+var accountpassword = "123456"
+accountcity = "jaipur"
+let accountstate;
+
+//accountId = 2 // not allowed 
+
+
+
+accountemail = "pr@gmail.com"
+accountpassword = "876543"
+accountcity = "pune"
+/*
+pleas not use var
+because of issue in block scope and functional scope
+
+*/
+
+console.log(accountId);
+console.table([accountId, accountemail,accountpassword ,accountcity,accountstate ])
---
+++ javascript01/datatype.js
@@ -0,0 +1,24 @@
+"use strict"; // treat all js code as newer version
+
+// alert (3+3) //we are using nodejs, not browser
+
+console.log(3
+    +
+    3)//code redability should be high 
+    console.log("pratik")
+
+    let name = "pratik"
+    let age = 24 
+    let isLoggedIn = false
+
+    //number => 2 to power 53
+    // bigint 
+    // string =>"" 
+     // boolean => true/false 
+     //null => standalone value 
+     //undefined =>
+
+
+     //object
+     console.log(typeof undefined);// output undefined
+     console.log(typeof null ); // output object
